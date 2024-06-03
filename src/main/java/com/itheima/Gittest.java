b656494bc00a4cb26ee6b65631aad04298ae463e
lh <1720901668@qq.com>
2024-06-03 15:11:46 +0800

    second commit

--- src/main/java/com/itheima/Gittest.java
+++ src/main/java/com/itheima/Gittest.java
@@ -3,5 +3,6 @@ package com.itheima;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
